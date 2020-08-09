97df1f44a4e04ab524fb415224b499425ccd64dd
Jonathan Trachtenberg <j.trachtenberg@gmail.com>
2020-08-08 17:35:16 -0700

    trigger endpoint on click

--- routes/unsplashapi.js
+++ routes/unsplashapi.js
@@ -9,5 +9,18 @@ module.exports = {
         console.log('api search call')
         console.log(keyword)
         return unsplash.search.photos(keyword, page, perpage).then(toJson)
+    },
+    async endpointTrigger({id}) {
+        console.log(id)
+        let retVal = await new Promise((resolve, reject) => 
+            unsplash.photos.getPhoto(id)
+            .then(toJson)
+            .then(json => {
+                let dp = unsplash.photos.downloadPhoto(json)
+                return resolve(dp)
+            })
+        )
+        console.log(retVal)
+        return retVal
     }
 }
