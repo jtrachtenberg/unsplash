24d34a0c9790c0f355ae306bcc2af860700f21be
Jonathan Trachtenberg <j.trachtenberg@gmail.com>
2020-08-13 15:51:08 -0700

    #pin pexel

--- routes/unsplashapi.js
+++ routes/unsplashapi.js
@@ -3,6 +3,8 @@ const toJson = require('unsplash-js').toJson
 const Unsplash = require('unsplash-js').default
 
 const unsplash = new Unsplash({accessKey: "T96xrE-u_EqE-WdvwR47aNL0QWd_CNAsZQKr6OJ0yF4"})
+const pexelAPI = "563492ad6f91700001000001a6c0fa6c901e4f748a4080172af215c5"
+//https://www.pexels.com/api/documentation/
 
 module.exports = {
     search({keyword,page,perpage}) {
