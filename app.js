afb0414de5955cabeafa281192b510e42dca003f
Jonathan Trachtenberg <j.trachtenberg@gmail.com>
2020-08-29 09:25:13 -0700

    audio support

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var unsplashRouter = require('./routes/unsplash')
+var freesoundRouter = require('./routes/freesound')
 
 global.fetch = require("node-fetch");
 
@@ -32,6 +33,10 @@ app.use('/users', usersRouter);
 app.use('/unsplash', unsplashRouter);
 app.use('/unsplash/search', unsplashRouter);
 app.use('/unsplash/endpointtrigger',unsplashRouter);
+app.use('/freesound', freesoundRouter);
+app.use('/freesound/search', freesoundRouter);
+app.use('/freesound/play', freesoundRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/freesoundapi.js
@@ -0,0 +1,52 @@
+//const Freesound = require('./freesoundmod')
+const token = "7hAy4HtMtOFDTOsTVMho7lp4wiu7EIOOevHdWHVb"
+const authHeader = `Token ${token}`
+const host = "https://freesound.org"
+const searchHost = `https://freesound.org/apiv2/search/text/?token=${token}&format=json&fields=id,name,description,previews,url,username&query=`
+const playHost = host+'/apiv2/sounds/'
+
+module.exports = {
+    async search({keyword, page}) {
+        page = page||1
+        console.log('freesoundapi search call')
+        console.log(keyword)
+        var retVal
+        const url = searchHost+keyword+'&page='+page
+        console.log(url)
+        console.log(url)
+        await fetch(url, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                }).then(response => response.json())
+                .then(data => {
+                    console.log('!!!!!')
+                    retVal = new Promise((resolve, reject) => resolve(data))
+                })
+                .catch(error => {
+                    console.log('error')
+                    retVal = new Promise((resolve) => resolve(error))
+                });
+
+        return retVal
+    },
+    async play ({id}) {
+        const url = playHost + id + '/?fields=id,name,description,previews,url,username&format=json&' + `token=${token}`
+        let retVal
+        await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then(response => response.json())
+        .then(data => {
+            retVal = new Promise((resolve, reject) => resolve(data))
+        })
+        .catch(error => {
+            retVal = new Promise((resolve) => resolve(error))
+        });
+
+        return retVal
+    }
+}
